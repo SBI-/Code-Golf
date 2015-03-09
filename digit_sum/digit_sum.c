92550a360e56fe86ec403e847132ca35d4119f28
SBI- <pascal.huerlimann@gmail.com>
2015-03-09 22:41:59 +0100

    started new code golf

---
+++ digit_sum/digit_sum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// http://codegolf.stackexchange.com/questions/47562/digit-sum-of-powers-in-bases
+int main(int argc, char* argv[])
+{
+	return EXIT_SUCCESS;
+}
