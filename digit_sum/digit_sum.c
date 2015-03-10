eae5f979e26b58de080cebfdb039ce307ea6bf01
Pascal <pascal.huerlimann@gmail.com>
2015-03-10 19:46:35 +0100

    Update digit_sum.c

--- digit_sum/digit_sum.c
+++ digit_sum/digit_sum.c
@@ -4,5 +4,6 @@
 // http://codegolf.stackexchange.com/questions/47562/digit-sum-of-powers-in-bases
 int main(int argc, char* argv[])
 {
+	int a = atoi(argv[1]), b = atoi(argv[2]), n = atoi(argv[3]);
 	return EXIT_SUCCESS;
 }
