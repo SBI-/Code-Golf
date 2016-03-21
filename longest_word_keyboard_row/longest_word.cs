0a74025f0f5342c3b41a7702c61462187a13ea14
SBI- <pascal.huerlimann@gmail.com>
2016-03-21 13:10:57 +0100

    Added a shorter version that doesn't use Where.

--- longest_word_keyboard_row/longest_word.cs
+++ longest_word_keyboard_row/longest_word.cs
@@ -1,2 +1,9 @@
 // http://codegolf.stackexchange.com/questions/75674/longest-word-using-a-single-row-of-the-qwerty-keyboard/75767#75767
 static void Main(string[]a){Console.WriteLine(a.Where(x=>Regex.IsMatch(x,"^([qwertzuiop]+|[asdfghjkl]+|[yxcvbnm]+)$")).OrderBy(x=>x.Length).Last());}
+
+
+// shorter version that doesn't use Where.
+static void f() 
+{
+    Console.WriteLine(a.OrderBy(x=>x.Length).Last(x=>Regex.IsMatch(x,"^([qwertzuiop]+|[asdfghjkl]+|[yxcvbnm]+)$")));
+}
