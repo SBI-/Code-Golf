bac29073dee02f17706fc9b5c394da11a9c636ac
SBI- <pascal.huerlimann@gmail.com>
2016-03-22 09:37:40 +0100

    Updated answer to be shorter as a full program than the java solution using only a single lambda.

--- longest_word_keyboard_row/longest_word.cs
+++ longest_word_keyboard_row/longest_word.cs
@@ -1,5 +1,5 @@
 // http://codegolf.stackexchange.com/questions/75674/longest-word-using-a-single-row-of-the-qwerty-keyboard/75767#75767
-static void Main(string[]a){Console.WriteLine(a.Where(x=>Regex.IsMatch(x,"^([qwertzuiop]+|[asdfghjkl]+|[yxcvbnm]+)$")).OrderBy(x=>x.Length).Last());}
+//static void Main(string[]a){Console.WriteLine(a.Where(x=>Regex.IsMatch(x,"^([qwertzuiop]+|[asdfghjkl]+|[yxcvbnm]+)$")).OrderBy(x=>x.Length).Last());}
 
 
 // shorter version that doesn't use Where.
@@ -7,3 +7,6 @@ static void f()
 {
     Console.WriteLine(a.OrderBy(x=>x.Length).Last(x=>Regex.IsMatch(x,"^([qwertzuiop]+|[asdfghjkl]+|[yxcvbnm]+)$")));
 }
+
+// full programm without using where.
+static void Main(string[]a){Console.WriteLine(a.OrderBy(x=>x.Length).Last(x=>Regex.IsMatch(x,"^([qwertzuiop]+|[asdfghjkl]+|[yxcvbnm]+)$")));}
